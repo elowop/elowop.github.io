166ddac379a25fa01c043f546d00d8c3ef650e05
elowop <42976129+elowop@users.noreply.github.com>
2023-02-23 18:21:20 -0500

    new entry

--- src/data/breadblog_entry.js
+++ src/data/breadblog_entry.js
@@ -1,26 +1,36 @@
 import challah2 from '../images/challah2.png';
 import challah1 from '../images/challah1.png';
-
+import hmbread1 from '../images/hmbread1.png'; 
 export default {
     entries: [
       {
         title: "Challah from Once Upon A Chef",
         link: "https://www.onceuponachef.com/recipes/challah.html",
-        description: ["honey is very pronounced compared to when using a preferment", 
-        "if you would like to reduce sweetness, substitute with an extra bit of oil to offset the liquid required",
-        "very easy to make without a dough hook or machine and only requires around 3 hours of resting time",
-        "a lot more dough-y and less airy on the inside"],
+        description: ["Honey is very pronounced compared to when using a preferment", 
+        "If you would like to reduce sweetness, substitute with an extra bit of oil to offset the liquid required",
+        "Very easy to make without a dough hook or machine and only requires around 3 hours of resting time",
+        "A lot more dough-y and less airy on the inside compared to when using a preferment"],
         rating: "6",
         image: challah1
       },
       {
         title: "Challah from NYT Cooking w/ Preferment",
         link: "https://cooking.nytimes.com/recipes/1023460-challah-bread",
-        description: ["using active yeast and creating a preferment caused a much more airy and pillow-y dough",
-         "would recommend using a stand or hand mixer and dough hook as it was very hard to create a well mixed dough with the preferment", 
-         "this recipe requires around 5-6 hours of resting so plan ahead when making the dough"],
+        description: ["Using active yeast and creating a preferment caused a much more airy and pillow-y dough",
+         "Would recommend using a stand or hand mixer and dough hook as it was very hard to create a well mixed dough with the preferment", 
+         "This recipe requires around 5-6 hours of resting so plan ahead when making the dough"],
         rating: "7",
         image: challah2
+      },
+      {
+        title: "Hokkaido Milk Bread by Diyar ",
+        link: "https://mildlymeandering.com/hokkaido-milk-bread-shokupan/",
+        description: ["It has a soft interior with a crispy, egg-brushed exterior makes for a nice dessert bread.",
+          "Making the starter (tangzhong) for the bread is tricky and requires delicate work with low heat",
+          "Be weary to use a piece of tin-foil if the egg-wash starts to burn",
+          "This recipe requires around 4 hours to make"],
+        rating: "7",
+        image: hmbread1
       }
     ]
   }
